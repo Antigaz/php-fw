35b415062afb1523f3ceaa7848ca6f35d89fd7c3
Antigaz <ludo.1610@hotmail.fr>
2018-01-18 17:23:05 +0100

    Form SignUp .. Vite fait quand même

---
+++ src/Metinet/Resources/views/signUp.html.php
@@ -0,0 +1,17 @@
+<form action="" method="post">
+    <label>Username
+        <input name="username" type="text" required>
+    </label>
+    <label>Password
+        <input name="password" type="password" required>
+    </label>
+    <label>Repeat password
+        <input name="repeat-password" type="password" required>
+    </label>
+    <label>Phone
+        <input name="phone" type="tel">
+    </label>
+    <label>E-mail
+        <input name="email" type="email">
+    </label>
+</form>
